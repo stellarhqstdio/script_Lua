e1f688d8bc2f363f695454a7bfbbae01a2a0c866
stellarhqstdio <stellarhqstudio@gmail.com>
2026-02-10 15:04:25 +0700

    ud afk

--- afk.lua
+++ afk.lua
@@ -1,13 +1,35 @@
-local afk_api = {}
+--จัดทำขึ้นโดย Stellar-HQ-Studio ห้ามแจกจ่ายหรือดัดแปรงทำซ้ำโดยเด็ดขาด--
 
-function  events.MOUSE_MOVE(x,y)
-    if x , y then
-        log("a")
-    else
-        log("b")
-    end
+local afk = {}
+
+local G = 0
+local tick_counter = 0
+
+local part = 
+
+function setpart(partmodel)
+   part = partmodel  
 end
 
+setpart(models.froghat.root.Head.bone)
+
+function events.entity_init()
+    log(part)
+    log("ลงทะเบียน model สำเร็จ")
+end
+
+--[[function events.tick(part)
+   tick_counter = tick_counter + 1
+   if tick_counter >= 40 then
+      G = G + 1
+      tick_counter = 0
+   end
+   log(part)
+   
+end
+]]--
 
 
-return afk_api
+
+
+return afk
